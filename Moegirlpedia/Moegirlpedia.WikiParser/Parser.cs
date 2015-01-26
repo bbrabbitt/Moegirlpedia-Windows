1ade839e197bf3403e1f7c4be791e7afb6498bac
Afa.L Cheng <alpha@tomatoeskit.org>
2015-01-25 22:09:01 -0600

    WikiText Helper class skeleton added.

---
+++ Moegirlpedia/Moegirlpedia.WikiParser/Parser.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using HtmlAgilityPack;
+
+namespace Moegirlpedia.WikiParser
+{
+    public class Parser
+    {
+        public void fuuu()
+        {
+            HtmlDocument doc = new HtmlDocument();
+        }
+    }
+}
---
+++ Moegirlpedia/UnitTestWikitextHelper/TestHTMLParser.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestWikitextHelper
+{
+    [TestClass]
+    public class TestHTMLParser
+    {
+        [TestMethod]
+        public void LoadHTMLTest()
+        {
+            
+        }
+    }
+}
